334ed170b5ac8f2515d075f478b397ae4359c9b5
reghuramk <reghuramk@users.noreply.github.com>
2020-09-02 23:01:13 +0530

    2_09_2020

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 600;
+canvas.height = 500;
+
+ctx.fillStyle =  "#30d03d";      
+ctx.fillRect(20, 20, 150, 100);
+ctx.fillRect(200, 20, 150, 100);
+
+
