a2eff6abae201fc40c5a52002aa518c922b952c4
ReghuramK <reghuramk04@gmail.com>
2020-09-03 13:56:29 +0530

    03_09_2020_laptop

--- JS/script.js
+++ JS/script.js
@@ -8,6 +8,11 @@ canvas.height = 500;
 
 ctx.fillStyle =  "#30d03d";      
 ctx.fillRect(20, 20, 150, 100);
+ctx.fillStyle =  "red";
 ctx.fillRect(200, 20, 150, 100);
 
+
+ctx.lineWidth = 5;
+ctx.strokeStyle =  "white";
+ctx.strokeRect(200, 200, 150, 100);
 
